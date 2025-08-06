6980a1166073c26bc2d353e68674ec3baa3f22a3
Ethan Saju <ethansaju10@gmail.com>
2025-08-06 15:57:52 +0530

    Initial Commit

---
+++ src/test/java/com/trak/trak/TrakApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trak.trak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrakApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
