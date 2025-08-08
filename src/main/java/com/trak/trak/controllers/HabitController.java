005efcc2fc985b83ae5d359355b9d6162ba96536
Ethan Saju <ethansaju10@gmail.com>
2025-08-08 16:46:16 +0530

    Completed Habit Controller

--- src/main/java/com/trak/trak/controllers/HabitController.java
+++ src/main/java/com/trak/trak/controllers/HabitController.java
@@ -1,8 +1,11 @@
 package com.trak.trak.controllers;
 
+import com.trak.trak.payload.APIResponse;
+import com.trak.trak.payload.habit.HabitCreateDTO;
 import com.trak.trak.payload.habit.HabitDTO;
 import com.trak.trak.services.HabitService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -24,7 +27,40 @@ public class HabitController {
             @PathVariable Long appUserId,
             @RequestParam("type") String type
     ) {
-        return new ResponseEntity<>(HabitService.getHabits(appUserId, type), HTTPStatus.OK);
+        return new ResponseEntity<>(habitService.getHabits(appUserId, type), HttpStatus.OK);
+    }
+
+    @GetMapping("/{appUserId}/{habitId}")
+    public ResponseEntity<HabitDTO> getHabit(
+            @PathVariable Long appUserId,
+            @PathVariable Long habitId
+    ) {
+        return new ResponseEntity<>(habitService.getHabit(appUserId, habitId), HttpStatus.OK);
+    }
+
+    @PostMapping("/{appUserId}")
+    public ResponseEntity<HabitDTO> createHabit(
+            @PathVariable Long appUserId,
+            @RequestBody HabitCreateDTO habitCreateDTO
+    ) {
+        return new ResponseEntity<>(habitService.createHabit(appUserId, habitCreateDTO), HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{appUserId}/{habitId}")
+    public ResponseEntity<HabitDTO> updateHabit(
+            @PathVariable Long appUserId,
+            @PathVariable Long habitId,
+            @RequestBody HabitCreateDTO habitCreateDTO
+    ) {
+        return new ResponseEntity<>(habitService.updateHabit(appUserId, habitId, habitCreateDTO), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{appUserId}/{habitId}")
+    public ResponseEntity<APIResponse> deleteHabit(
+            @PathVariable Long appUserId,
+            @PathVariable Long habitId
+    ) {
+        return new ResponseEntity<>(habitService.deleteHabit(appUserId, habitId), HttpStatus.OK);
     }
 
 
