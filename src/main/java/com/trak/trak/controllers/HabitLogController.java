3f9021794677798ec5e00b2c2554ae71f00857d8
R S Chiraag <chiraag.rs@gmail.com>
2025-08-18 18:04:26 +0530

    Add habit log controller

--- src/main/java/com/trak/trak/controllers/HabitLogController.java
+++ src/main/java/com/trak/trak/controllers/HabitLogController.java
@@ -1,14 +1,46 @@
 package com.trak.trak.controllers;
 
+import com.trak.trak.payload.habitLog.HabitLogDTO;
+import com.trak.trak.payload.habitLog.HabitLogUpdateDTO;
+import com.trak.trak.services.HabitLogService;
+import org.springframework.boot.autoconfigure.graphql.GraphQlProperties;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/habitLogs")
 public class HabitLogController {
+    private final HabitLogService habitLogService;
 
-    @GetMapping
-    public ResponseEntity<String> getHabitLogs() {
+    public HabitLogController(HabitLogService habitLogService) {
+        this.habitLogService = habitLogService;
+    }
+
+    @GetMapping("/{appUserId}")
+    public ResponseEntity<List<HabitLogDTO>> getHabitLogs(@PathVariable Long appUserId) {
+        return new ResponseEntity<>(habitLogService.getHabitLogs(appUserId), HttpStatus.OK);
+    }
+
+    @PatchMapping("/{appUserId}/habitLogID")
+    public ResponseEntity<HabitLogDTO> updateHabitLog(
+            @PathVariable Long appUserId,
+            @PathVariable Long habitLogId,
+            @RequestBody HabitLogUpdateDTO habitLogUpdateDTO
+    ) {
+        return new ResponseEntity<>(habitLogService.updateHabitLog(appUserId, habitLogId, habitLogUpdateDTO), HttpStatus.OK);
+    }
+
+    @GetMapping("/{appUserId}/{habitId}")
+    public ResponseEntity<List<HabitLogDTO>> getHabitLogsByDate(
+            @PathVariable Long appUserId,
+            @PathVariable Long habitId,
+            @RequestParam String startDate,
+            @RequestParam String endDate
+    ) {
+        return new ResponseEntity<>(habitLogService.getHabitLogsByDate(appUserId, habitId, startDate, endDate), HttpStatus.OK);
+    }
 }
